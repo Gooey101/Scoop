e3ea31d483304bf8b73cf21da2fe8d71c5e94460
Christopher Gu <clg2n4@mst.edu>
2017-10-14 12:14:40 -0500

    updated Report class and created "Sign In" related files

--- app/src/main/java/com/example/cleeg/scoopreporter/Report.java
+++ app/src/main/java/com/example/cleeg/scoopreporter/Report.java
@@ -5,23 +5,23 @@ public class Report {
     private String mTitle;
     private String mInformation;
     private Integer mDate;
-    private Reporter mReporter;
+    private String mReporterKey;
     private String mOrganization;
 
     public Report() {}
 
-    public Report(String title, String information, Integer date, Reporter reporter,
+    public Report(String title, String information, Integer date, String reporterKey,
                   String organization) {
         mTitle = title;
         mInformation = information;
         mDate = date;
-        mReporter = reporter;
+        mReporterKey = reporterKey;
         mOrganization = organization;
     }
 
     public String getTitle() { return mTitle; }
     public String getInformation() { return mInformation; }
     public Integer getDate(){ return mDate; }
-    public Reporter getReporter() { return mReporter; }
+    public String getReporterKey() { return mReporterKey; }
     public String getOrganization() { return mOrganization; }
 }
